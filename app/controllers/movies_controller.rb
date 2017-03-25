b68198610757297b0e780d3c4e7f00292786b401
zhedian <cooldeer@126.com>
2017-03-25 21:33:43 +0800

    when created a movie, like it automatically

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,6 +37,7 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
     @movie.user = current_user
     if @movie.save
+      current_user.like!(@movie)
       redirect_to movies_path
     else
       render :new
