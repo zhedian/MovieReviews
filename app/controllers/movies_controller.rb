7465a96b3d62a14724dab2e76525e8517ff2f31d
zhedian <cooldeer@126.com>
2017-03-25 16:11:25 +0800

    implement movies#create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,19 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :description)
+  end
+
+
 end
