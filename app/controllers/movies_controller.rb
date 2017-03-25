d2ce9202dce25aafe42121eff7165d9ccba35952
zhedian <cooldeer@126.com>
2017-03-25 16:19:24 +0800

    implement movies#edit&update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+
+    redirect_to movies_path 
+  end
+
   def new
     @movie = Movie.new
   end
