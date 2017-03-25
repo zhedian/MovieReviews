2bdbe9d1a60d43d8ed9c24b11f14da465c006543
zhedian <cooldeer@126.com>
2017-03-25 18:37:41 +0800

    implement movie relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
   has_many :reviews
+  has_many :movie_relationships
+  has_many :fans, through: :movie_relationships, source: :user
 end
